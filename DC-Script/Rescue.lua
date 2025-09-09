06b0dd950b42b10822dd95f2671d0d4c522a2511
Jay mark <lpatikim01@gmail.com>
2025-09-10 07:29:00 +0800

    Update Rescue.lua

--- DC-Script/Rescue.lua
+++ DC-Script/Rescue.lua
@@ -1,3 +1,20 @@
+
+L={'◼', '◻', '◻', '◻', '◻', '◻'} -- LOADING TABLE
+LD = 0
+for i=0, 6, 1 do
+	if gg.isVisible(true) and i ~= 6 then
+		gg.setVisible(false)
+	end
+	gg.sleep(1000)
+	gg.toast('Loading: '..L[1]..L[2]..L[3]..L[4]..L[5]..L[6]..' '..LD..'/100%')
+	LD = LD + 20
+	table.remove(L)
+	table.insert(L, 2, "◼")
+	if i == 6 then
+		gg.sleep(2000)
+	end
+end
+
 gg.setRanges(gg.REGION_C_ALLOC)
 gg.searchNumber('1000~5600;-1;1~70;0~5::17', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
 gg.refineNumber('1000~5600;-1;1~70::10', gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
